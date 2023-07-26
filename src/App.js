41f427222ca4f7c4edd98eecbc944db411bb5800
Justgodfather <nurbanbt@gmail.com>
2023-07-26 20:48:36 +0600

    first

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import AddTodo from "./components/AddTodo/AddTodo";
+import TodoList from "./components/TodoList/TodoList";
+import EditTodo from "./components/EditTodo/EditTodo";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [editedObj, setEditedObj] = useState(null);
+
+  function handleTask(newObj) {
+    const newTodos = [...todos];
+
+    newTodos.push(newObj);
+
+    setTodos(newTodos);
+  }
+
+  function changeStatus(id) {
+    let newTodos = todos.map((item) => {
+      if (item.id === id) {
+        item.status = !item.status;
+        return item;
+      } else {
+        return item;
+      }
+    });
+
+    setTodos(newTodos);
+  }
+
+  function getEditedTask(id) {
+    let oneObj = todos.find((item) => item.id === id);
+
+    setEditedObj(oneObj);
+  }
+
+  function saveChanges(newObj) {
+    let newTodos = [...todos];
+
+    newTodos = newTodos.map((item) => {
+      if (item.id === newObj.id) {
+        return newObj;
+      } else {
+        return item;
+      }
+    });
+
+    setTodos(newTodos)
+
+    setEditedObj(null)
+  }
+
+  function deleteTask(id) {
+    let newTodos = [...todos]
+
+    newTodos = newTodos.filter((item) => item.id !== id)
+
+    setTodos(newTodos)
+  }
+
+  return (
+    <>
+      <AddTodo handleTask={handleTask} />
+      <TodoList
+        todos={todos}
+        changeStatus={changeStatus}
+        getEditedTask={getEditedTask}
+        deleteTask={deleteTask}
+      />
+      {editedObj ? (
+        <EditTodo editedObj={editedObj} saveChanges={saveChanges} />
+      ) : (
+        <h4>Not editing todo now</h4>
+      )}
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./TodoList.css";
+
+const TodoList = ({ todos, changeStatus, getEditedTask, deleteTask }) => {
+  let style = {
+    color: "red",
+    listStyleType: "none",
+  };
+
+  return (
+    <>
+      <h2>TODO List Component</h2>
+      <ul style={style}>
+        {todos.map((item) => (
+          <li key={item.id} className={item.status ? 'complited' : ""}>
+            <input type="checkbox" onChange={(() => changeStatus(item.id))}/>
+            {item.task}
+            <button onClick={() => getEditedTask(item.id)}>Edit Task</button>
+            <button onClick={() => deleteTask(item.id)}>Delete Task</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default TodoList;
